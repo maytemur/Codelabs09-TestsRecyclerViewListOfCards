efca82e3ef5669099f1aa5eec6fa458a11628290
Mustafa Aytemür <maytemur@gmail.com>
2023-03-12 02:19:33 +0300

    Codelabs Test RecyclerView List of Cards Google Kotlin ilk commit

---
+++ app/src/main/java/com/maytemur/affirmations/model/Affirmation.kt
@@ -0,0 +1,12 @@
+package com.maytemur.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+) {
+
+
+}
